11d41bef2eda2d1de220b412f9d6c1ad3648c0f1
NatalieMonique111 <nataliemonique111@gmail.com>
2021-08-27 14:16:50 -0700

    submission

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,30 +2,56 @@ import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
+
+
 const PERMISSIONS = ['Yes', 'No'];
 
 const RegisterYourApplicationForm = () => {
-    const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date());
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    age: '',
+    bool: '',
+  });
+
+  const set = (name) => {
+    return ({ target: { value } }) => {
+      setValues((oldValues) => ({ ...oldValues, [name]: value }));
+    };
+  };
+
+  const onSubmit = (event) => {
+    const YYYY = date.getFullYear();
+    let MM = date.getMonth() + 1;
+    let DD = date.getDate();
+    event.preventDefault();
+    console.log(`Name:${values.name} Email:${values.email} Age:${values.age}`);
+    console.log(`Month:${MM} Day:${DD} Year:${YYYY}`)
+    console.log(`${values.bool}`)
+
+  };
+
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h2>Please Register for our next Event</h2>
 
       <label>Name*:</label>
-      <input type="text" required placeholder="Name" />
+      <input type="text" value={values.name} onChange={set('name')} required placeholder="Name" />
 
       <label>Email*:</label>
-      <input type="email" required placeholder="Email"/>
+      <input type="email" value={values.email} onChange={set('email')} required placeholder="Email" />
 
       <label>Age*:</label>
-      <input type="number" required min="0" placeholder="Age" />
+      <input type="number" value={values.age} onChange={set('age')} required min="0" placeholder="Age" />
 
       <label>Select a Date*:</label>
-      <DatePicker required placeholderText="Select Date" selected={date} onChange={date => setDate(date)} />
+      <DatePicker placeholderText="Select Date" value={date} selected={date} onChange={date => setDate(date)} />
 
       <label>About us*:</label>
       <select required>
-        <option value="">Do you want to receive Techtonica's Newsletter</option>
+        <option value={values.bool}>Do you want to receive Techtonica's Newsletter</option>
         {PERMISSIONS.map(preference => <option key={preference}>{preference}</option>)}
       </select>
 
