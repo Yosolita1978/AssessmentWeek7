2b88f6514e235ccfd4321ae44599b3a14a1c9f01
Cristina Rodriguez <yosola@gmail.com>
2021-08-30 14:05:43 -0700

    Solution implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './components/Form';
+import React, { useState } from "react";
 import './App.css';
 import RegisterYourApplicationForm from './components/Form';
 
 function App() {
+
+  // a state to control if the user is register or not. This allows the parent to control the flow of the application 
+  const [isSubmited, setIsSubmited] = useState(false);
+
+  //sending this function as a prop to the child 
+  const handleOnSubmit = () => {
+    setIsSubmited(true);
+    console.log("Submited value change in parent")
+  }
+
   return (
     <div className="App">
-      <RegisterYourApplicationForm />
+      {isSubmited ? 
+      <div className="sucess-message"> Sucess! Thank you for registering for our event </div> : <RegisterYourApplicationForm isSubmited={isSubmited} onSubmit={handleOnSubmit} />} 
+      
+  
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,29 +4,63 @@ import "react-datepicker/dist/react-datepicker.css";
 
 const PERMISSIONS = ['Yes', 'No'];
 
-const RegisterYourApplicationForm = () => {
-    const [date, setDate] = useState(new Date());
+const RegisterYourApplicationForm = (props) => {
+
+  const [date, setDate] = useState(new Date());
+
+  //create a state for the values that you need in the form (I create an object with those values)
+  const [valuesForm, setValuesForm] = useState({
+    userName: "",
+    userEmail: "",
+    userAge: "",
+    userPreference: ""
+  });
+  
+  //create functions that handle the event of the user typing into the form 
+  const handleInputNameChange = (event) => {
+    setValuesForm({...valuesForm, userName: event.target.value})
+  }
+
+  const handleInputEmailChange = (event) => {
+    setValuesForm({...valuesForm, userEmail: event.target.value})
+  }
+
+  const handleInputAgeChange = (event) => {
+    setValuesForm({...valuesForm, userAge: event.target.value})
+  }
+
+  const handleInputPreferenceChange = (event) => {
+    setValuesForm({...valuesForm, userPreference: event.target.value})
+  }
+
+  //create a function to handle the submit of the form to the parent component 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.onSubmit()
+    console.log(valuesForm);
+  }
+
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Please Register for our next Event</h2>
 
-      <label>Name*:</label>
-      <input type="text" required placeholder="Name" />
+      <label htmlFor="Name:">Name:*</label>
+      <input type="text" required placeholder="Name" value={valuesForm.userName} onChange={handleInputNameChange} />
 
-      <label>Email*:</label>
-      <input type="email" required placeholder="Email"/>
+      <label htmlFor="Email:">Email*:</label>
+      <input type="email" required placeholder="Email" value={valuesForm.userEmail} onChange={handleInputEmailChange}/>
 
-      <label>Age*:</label>
-      <input type="number" required min="0" placeholder="Age" />
+      <label htmlFor="Age:">Age*:</label>
+      <input type="number" required min="0" placeholder="Age" value={valuesForm.userAge} onChange={handleInputAgeChange} />
 
-      <label>Select a Date*:</label>
-      <DatePicker required placeholderText="Select Date" selected={date} onChange={date => setDate(date)} />
+      <label htmlFor="Date:">Select a Date*:</label>
+      <DatePicker required placeholderText="Select Date" selected={date} onChange={(date => {setDate(date)})} />
 
-      <label>About us*:</label>
-      <select required>
-        <option value="">Do you want to receive Techtonica's Newsletter</option>
-        {PERMISSIONS.map(preference => <option key={preference}>{preference}</option>)}
+      <label htmlFor="Preference:">About us*:</label>
+      <select onChange={handleInputPreferenceChange} required>
+        <option value={valuesForm.userPreference}>Do you want to receive Techtonica's Newsletter</option>
+        {PERMISSIONS.map(preference => <option key={preference} value={preference}>{preference}</option>)}
       </select>
 
       <button type="submit">Submit</button>
